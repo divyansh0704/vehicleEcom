a3f25d47c522a71b906edd7b572237012dc796b6
divyansh0704 <divyansh298.be22@chitkara.edu.in>
2026-01-14 21:13:06 +0530

    backend reset

--- client/src/api/api.jsx
+++ client/src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:"https://vehicleecom.onrender.com/api"
+    baseURL:"https://vehicleecom-sw46.onrender.com/api"
 })
 
 export default api;
